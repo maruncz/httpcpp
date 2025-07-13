6121c0d42656fdbe06e8df53355d537c78cb6ca2
maruncz <marun1@email.cz>
2025-07-13 08:04:38 +0200

    fix project structure

--- build/CPM_modules/FindPackageProject.cmake.cmake
+++
@@ -1,3 +0,0 @@
-include(/app/build/cmake/CPM_0.31.1.cmake)
-CPMAddPackage(NAME;PackageProject.cmake;GITHUB_REPOSITORY;TheLartians/PackageProject.cmake;VERSION;1.6.0)
-set(PackageProject.cmake_FOUND TRUE)
